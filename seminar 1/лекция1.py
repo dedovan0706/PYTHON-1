2a468a27e984873dc9e58f794eea9435180c0289
dedov <dedovan0706@gail.com>
2022-09-25 11:06:12 +0700

    Add seminar 1

---
+++ seminar 1/лекция1.py
@@ -0,0 +1,27 @@
+# Ввод и вывод данных
+# print('Введите а');
+# a = input() # 10
+# print('Введите b');
+# b = input() # 20
+# c = 30
+# print(a, ' + ', b, ' = ', c)
+
+# print('Введите а');
+# a = input() # 10
+# print('Введите b');
+# b = input() # 20
+# c = 30
+# print(a, ' + ', b, ' = ', c) # 10 + 20 = 30
+# print('Введите а');
+# a = int(input())
+# print('Введите b');
+# b = int(input())
+# c = 30
+# print(a, ' + ', b, ' = ', c)
+# print('{2} + {0} = {1}'.format(a, b, c))
+print('Введите а');
+a = float(input())
+print('Введите b');
+b = float(input())
+c = ...
+print(a, ' + ', b, ' = ', c)
